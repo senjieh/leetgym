ce6b048e18a557391333e5c43f281aaaba5637f9
Jie Sen Huang <71396823+senjieh@users.noreply.github.com>
2024-12-30 07:11:32 -0800

    Init: Initial Template and Empty Q1

---
+++ template.py
@@ -0,0 +1,21 @@
+"""
+
+
+    @type  a: type
+    @param m: description
+    @type  b: type
+    @param b: description
+    @rtype:   type
+    @return:  description
+
+
+"""
+
+def testCases():
+    assert(solution() == "")
+    assert(solution() == "")
+    assert(solution() == "")
+    assert(solution() == "")
+
+def solution() -> int:
+    return
